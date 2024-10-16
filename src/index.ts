5b0b21cfb73e1ccedebcbbb321e97996107dc9e4
Aryan Kumar <aryan.ak434@gmail.com>
2024-10-16 13:48:01 +0530

    feat: reuse

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
+async function _getTokenFromId(id: string, account_id: string, api_key: string): Promise<string> {
+	const options = {
+		method: 'GET',
+		headers: {'Content-Type': 'application', 'Authorization': `Bearer ${api_key}`}
+	};
+	let response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${account_id}/cfd_tunnel/${id}/token`, options);
+	let data: any = await response.json();
+	return data.result;
+};
+
+async function _getTunnelToken(name: string, account_id: string, api_key: string): Promise<string> {
+	const options = {
+		method: 'GET',
+		headers: {'Content-Type': 'application', 'Authorization': `Bearer ${api_key}`}
+	};
+	let response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${account_id}/cfd_tunnel`, options);
+	let data: any = await response.json();
+	let tunnels: any[] = data.result;
+	for (let i = 0; i < tunnels.length; i++) {
+		if (tunnels[i].name === name) {
+			let id: string = tunnels[i].id;
+			return await _getTokenFromId(id, account_id, api_key);
+		}
+	}
+	return '';
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		// @ts-ignore
@@ -8,17 +35,25 @@ export default {
 		if (!name) {
 			return new Response(`echo "connector name required"`, { status: 400 });
 		}
+
+		let installUfwScript: string = `sudo pacman -S ufw`;
+		let allowPort22: string = `sudo ufw allow 22/tcp`;
+		let serviceInstall: string = `sudo cloudflared service install`;
+		let cloudflaredInstall: string = `sudo pacman -S cloudflared`;
 		name = "qt_" + name;
+
+		let tunnelToken: string = await _getTunnelToken(name, account_id, api_key);
+		if (tunnelToken) {
+			return new Response(`${installUfwScript} && \n${allowPort22} && \n${cloudflaredInstall} && \n${serviceInstall} ${tunnelToken}\n`, { status: 200 });
+		}
 		const options = {
 			method: 'POST',
 			headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${api_key}`},
 			body: `{"config_src":"cloudflare","name":"${name}","tunnel_secret":"bXlzZWNyZXRwYXNzZm9yY2xvdWRmbGFyZQ=="}`
 		};
-		let response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${account_id}/cfd_tunnel`, options);
+		let response: any = await fetch(`https://api.cloudflare.com/client/v4/accounts/${account_id}/cfd_tunnel`, options);
 		let data: any = await response.json();
-		let token: string = data.result.token;
-		let script: string = `curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared.deb && sudo cloudflared service install ${token}`;
-
-		return new Response(script, { status: 200 });
+		tunnelToken = data.result.token;
+		return new Response(`${installUfwScript} && \n${allowPort22} && \n${cloudflaredInstall} && \n${serviceInstall} ${tunnelToken}\n`, { status: 200 });
 	},
 } satisfies ExportedHandler<Env>;
